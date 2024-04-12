baa1bb43fb86b620ab6c6c1410d7b2a839c01953
Mario Cuaya <mariocuaya5@gmail.com>
2024-04-11 23:29:33 -0700

    Adding small wrapper script to activate python virtual environment inside of the container

---
+++ helper_functions/pymods.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Simple wrapper script to activate the python virtual enviroment containing the necessary
+#python packages from the pip installer.
+. /python-venvs/python-pip-modules/bin/activate
+
