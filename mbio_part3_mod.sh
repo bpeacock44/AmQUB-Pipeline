a9e09af3417985bcfb4a87098e81ec38e595c017
Beth Peacock <bethpeacock@Beths-MBP.attlocal.net>
2024-06-20 12:49:24 -0700

    minor update

--- mbio_part3_mod.sh
+++ mbio_part3_mod.sh
@@ -164,6 +164,16 @@ mkdir -vp "${output_dir}/asvs"
 # MODIFIED LINE - CLUSTERING at 100% IDENTITY
 usearch -cluster_smallmem "${output_dir}/uniques.fa" -id 1 -centroids "${output_dir}/asvs/asvs.fa"
 
+# Convert headers
+sed 's/>Uniq\([0-9]*\);.*$/>Asv\1;/' "${output_dir}/asvs/asvs.fa" > "${output_dir}/asvs/z.fa"
+
+# Check if the replacement was successful before overwriting
+if grep -q '>Asv' "${output_dir}/asvs/z.fa"; then
+    # Overwrite the original file if '>Asv' is found
+    mv -v "${output_dir}/asvs/z.fa" "${output_dir}/asvs/asvs.fa"
+else
+    echo "Header replacement failed. Original file not overwritten."
+fi
 echo
 echo " - -- --- ---- ---- --- -- -"
 echo "Creating Initial ASV Table"
