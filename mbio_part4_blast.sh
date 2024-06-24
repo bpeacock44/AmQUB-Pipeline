710bf1b7538cc6215eb1495fb980c04fefd6bddb
Beth Peacock <bethpeacock@Beths-MBP.attlocal.net>
2024-06-24 09:51:16 -0700

    minor update

--- mbio_part4_blast.sh
+++ mbio_part4_blast.sh
@@ -631,6 +631,8 @@ outfp="${output_dir}/asvs/asv_table_03_add_seqs.txt"
 ls $otblfp
 ls ${output_dir}'/asvs/rep_set/seqs_chimera_filtered_ASVs.fasta'
 echo $outfp
+echo "Rscript -e \"source('${HDIR}/pipeline_helper_functions.R'); add_sequences_to_asv_table('${otblfp}', '${output_dir}/asvs/rep_set/seqs_chimera_filtered_ASVs.fasta', '${outfp}')\""
+
 
 Rscript -e "source('${HDIR}/pipeline_helper_functions.R'); add_sequences_to_asv_table('$otblfp', '${output_dir}/asvs/rep_set/seqs_chimera_filtered_ASVs.fasta', '$outfp')"
 
