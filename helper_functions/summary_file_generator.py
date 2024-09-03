0cda9eda4266c4bc05fb0fcd765a539ac316c832
Beth Peacock <bethpeacock@Beths-MacBook-Pro.local>
2024-09-03 16:36:46 -0500

    removing_all_R_dependencies

--- helper_functions/summary_file_generator.py
+++ helper_functions/summary_file_generator.py
@@ -10,29 +10,24 @@ def process_data_and_write_summary(norm, raw, tax, tax_c, mixed, output_file):
     df.rename(columns={df.columns[0]: 'ASV_ID'}, inplace=True)
     if 'taxonomy' in df.columns:
         df.drop(columns=['taxonomy'], inplace=True)
-
     # Read primary taxonomy file
     tax_df = pd.read_csv(tax, sep='\t', comment=None, header=0)
     tax_df.columns = ['ASV_ID', 'taxonomy', 'bitscore', 'per_ID', 'per_qcov']
     df = pd.merge(df, tax_df[['ASV_ID', 'taxonomy', 'per_ID', 'per_qcov']], on='ASV_ID', how='left')
-
     # Check if tax_c is provided and process it if available
     if tax_c and tax_c.lower() != 'null':
         # Read classifier taxonomy file if given
         tax_df_C = pd.read_csv(tax_c, sep='\t', comment=None, header=0)
         tax_df_C.columns = ['ASV_ID', 'c_taxonomy', 'confidence']
         df = pd.merge(df, tax_df_C, on='ASV_ID', how='left')
-
         # Include c_taxonomy and confidence in excluded columns
         excluded_columns = ['ASV_ID', 'taxonomy', 'per_ID', 'per_qcov', 'c_taxonomy', 'confidence', 'sequence']
     else:
         # Exclude c_taxonomy and confidence if tax_c is not provided
         excluded_columns = ['ASV_ID', 'taxonomy', 'per_ID', 'per_qcov', 'sequence']
-
     # Calculate mean abundance using only numeric columns
     numeric_cols = df.select_dtypes(include=[np.number]).columns.difference(excluded_columns)
     df['avg_abun'] = df[numeric_cols].mean(axis=1, skipna=True)
-
     # Flag ASVs with mixed families in top 10
     top10_df = pd.read_csv(mixed, sep='\t', comment=None, header=None)
     df['mixed'] = np.where(df['ASV_ID'].isin(top10_df.iloc[:, 0]), 'yes', 'no')
@@ -45,7 +40,7 @@ def process_data_and_write_summary(norm, raw, tax, tax_c, mixed, output_file):
 
     # Add row with total raw counts
     rawdf = pd.read_csv(raw, sep='\t', comment=None, header=0)
-    rawdf.drop(columns=[rawdf.columns[0], 'taxonomy', 'sequence'], inplace=True, errors='ignore')
+    rawdf.drop(columns=[rawdf.columns[0], 'taxonomy', 'Sequence'], inplace=True, errors='ignore')
     sums = rawdf.sum()
 
     if tax_c and tax_c.lower() != 'null':
